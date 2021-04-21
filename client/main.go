19e6a8fc85c44485c793c163510d1e070b5717af
Oli <olifer97@gmail.com>
2021-04-21 17:25:55 -0300

    fix border cases

--- client/main.go
+++ client/main.go
@@ -10,47 +10,90 @@ import (
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/common"
 )
 
-// InitConfig Function that uses viper library to parse env variables. If
-// some of the variables cannot be parsed, an error is returned
-func InitConfig() (*viper.Viper, error) {
+type ConfigJSON struct {
+    CLI_ID string `json:"CLI_ID"`
+    CLI_SERVER_ADDRESS string `json:"CLI_SERVER_ADDRESS"`
+    CLI_LOOP_LAPSE string `json:"CLI_LOOP_LAPSE"`
+    CLI_LOOP_PERIOD string `json:"CLI_LOOP_PERIOD"`
+}
+
+// InitConfig Function that uses viper library to parse env variables or config file. 
+// Returns ClientConfig struct.
+// If some of the variables cannot be parsed, an error is returned
+func InitConfig() (*common.ClientConfig, error) {
 	v := viper.New()
 
-	// Configure viper to read env variables with the CLI_ prefix
-	v.AutomaticEnv()
-	v.SetEnvPrefix("cli")
+	v.SetConfigName("client") 
+	v.AddConfigPath("./config")  
+	
+	var configuration ConfigJSON
 
-	// Add env variables supported
-	v.BindEnv("id")
-	v.BindEnv("server", "address")
-	v.BindEnv("loop", "period")
-	v.BindEnv("loop", "lapse")
+	v.AutomaticEnv()
+
+	err_read := v.ReadInConfig()
+
+	err_unmarshal := v.Unmarshal(&configuration)
+
+	if err_read != nil && err_unmarshal != nil  {
+		return nil, errors.Wrapf(err_read, "Could not parse config variables")
+	}
+
+	var id string
+	if v.IsSet("CLI_ID") {
+		id = v.GetString("CLI_ID")
+	} else {
+		id = configuration.CLI_ID
+	}
+
+	var server_address string
+	if v.IsSet("CLI_SERVER_ADDRESS") {
+		server_address = v.GetString("CLI_SERVER_ADDRESS")
+	} else {
+		server_address = configuration.CLI_SERVER_ADDRESS
+	}
+
+	var loop_lapse string
+	if v.IsSet("CLI_LOOP_LAPSE") {
+		loop_lapse = v.GetString("CLI_LOOP_LAPSE")
+	} else {
+		loop_lapse = configuration.CLI_LOOP_LAPSE
+	}
+
+	duration_loop_lapse, err := time.ParseDuration(loop_lapse)
 
 	// Parse time.Duration variables and return an error
 	// if those variables cannot be parsed
-	if _, err := time.ParseDuration(v.GetString("loop_lapse")); err != nil {
-		return nil, errors.Wrapf(err, "Could not parse CLI_LOOP_LAPSE env var as time.Duration.")
+	if err != nil {
+		return nil, errors.Wrapf(err, "Could not parse CLI_LOOP_LAPSE config var as time.Duration.")
 	}
 
-	if _, err := time.ParseDuration(v.GetString("loop_period")); err != nil {
-		return nil, errors.Wrapf(err, "Could not parse CLI_LOOP_PERIOD env var as time.Duration.")
+	var loop_period string
+	if v.IsSet("CLI_LOOP_PERIOD") {
+		loop_period = v.GetString("CLI_LOOP_PERIOD")
+	} else {
+		loop_period = configuration.CLI_LOOP_PERIOD
 	}
 
-	return v, nil
+	duration_loop_period, err := time.ParseDuration(loop_period)
+
+	if err != nil {
+		return nil, errors.Wrapf(err, "Could not parse CLI_LOOP_PERIOD config var as time.Duration.")
+	}
+
+	return &common.ClientConfig{
+		ServerAddress: server_address,
+		ID:            id,
+		LoopLapse:     duration_loop_lapse,
+		LoopPeriod:    duration_loop_period,
+	}, nil
 }
 
 func main() {
-	v, err := InitConfig()
+	clientConfig, err := InitConfig()
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
 
-	clientConfig := common.ClientConfig{
-		ServerAddress: v.GetString("server_address"),
-		ID:            v.GetString("id"),
-		LoopLapse:     v.GetDuration("loop_lapse"),
-		LoopPeriod:    v.GetDuration("loop_period"),
-	}
-
-	client := common.NewClient(clientConfig)
+	client := common.NewClient(*clientConfig)
 	client.StartClientLoop()
 }
